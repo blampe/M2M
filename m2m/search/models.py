4e70b29ba9359bc2467609096a9023dbc96ebec4
Haak Saxberg <haak@Artemis.local>
2011-04-21 15:21:52 -0700

    Added tmdb to repo, as a git subrepo; also improved the movie scraper. Not sure if it works totally yet....

--- m2m/search/models.py
+++ m2m/search/models.py
@@ -77,47 +77,83 @@ class Movie(models.Model):
         
         # grab all video files from things with Movie in the path name,
         # excluding things whose filename begin with '.' or '_'
-        candidates = []
-        for fileType in File.videoEndings.split('|'):
-            candidates += File.objects.exclude(path__fullname__icontains='Pornography')\
-                            .filter(path__fullname__icontains='Movies')\
-                            .filter(filenameend__iexact=fileType)\
-                            .exclude(filename__istartswith='.')\
-                            .exclude(filename__istartswith='_')
-            
+        candidates = File.objects.filter(filenameend__regex=r'(%s)' % File.videoEndings)
+        print "Filtered out non-(%s)" % File.videoEndings
+        
+        dirExcludes = "pornography"
+        
+        candidates = candidates.exclude(path__fulname__regex='(%s)' % dirExcludes)\
+                               .filter(path__fullname__icontains='Movies')\
+                               .exclude(filename__istartswith='.')\
+                               .exclude(filename__istartswith='_')
+        
+        print "Filtered out things in (%s) directories, things not in movies" % dirExcludes
+        
         # We should now have all likely video files.
         # Filter according to the regexp
         # (.)*( \((([a-zA-Z]) (- )?)?[12][0-9][0-9][0-9]\)\)?.(.)*
         # Filename[ ([Director [- ]]Year)].filenameend
         # so that we can use this shit with tmdb/imdb
+        print "narrowing down filenames a little further..."
         candidates.filter(filename__regex=r'(.)+( \(([a-zA-Z]* (- )?)?[12][0-9][0-9][0-9]\)\))?.(.)*')
         
         for candidate in candidates:
+            # get rid of the file extension
             sliceIndex = candidate.filename.rfind('.')
             info = candidate.filename[:sliceIndex]
-            info = re.split("\((.*)\)\Z",info)
+            
+            # some people (coughWOPRcough) like to use '\.' instead of spaces, in their filenames.
+            # fuck those people.
+            
+            info = re.split("\.",info)
+            info = " ".join(info)
+            info = re.split("\((.*)\)",info)
+            
             probablyTitle = info[0].rstrip()
             # get some meta-data
+            print "Stripping metadata out of title, if it's there."
             if len(info) > 1:
                 diryear = re.search('((?P<DIRECTOR>(.+)) - )?(?P<YEAR>\d{4})', info[1])
                 try: # if there's a year
                     year = diryear.group('YEAR')
+                    print "Found year data."
                 except KeyError:
                     year = ""
+            
                 try: # if there's a director
                     director = diryear.group('DIRECTOR')
+                    print "Found director data."
                 except KeyError:
                     director = ""
+            else:
+                year = ""
+                director = ""
                     
             # find movies that match the title
+            print "Querying TMDB... (%s) " % probablyTitle
             movies = search("%s %s" % (probablyTitle, year))
+            
+            if len(movies) > 0:
+                print "Found something!"
+                                   
             for movieresult in movies:
                 # now, get the info and put in the DB - if it's not already there.
                 try:
                     checker = Movie.objects.get(pk=int(movieresult['id']))
-                    continue
+                    print "Movie already in database; no new entry made."
+                    print "Checking to see if this is a new host..."
+                    if candidate in checker.FIDs:
+                        print "Not a new file, moving on."
+                        continue
+                    else:
+                        print "New file! adding to list of sources..."
+                        checker.FIDs += candidate
+                        checker.save()
                 except:
                     movie = getMovieInfo(movieresult['id'])
+                    print "Movie not in database:"
+                    print "\t Found: %s" % movie['name']
+                    
                     '''latestEntry = Movie(
                                 rating=movie['rating'],
                                 votes=movie['votes'],
