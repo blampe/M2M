d7f282274836f3fdb6798d6e895ed642778b3048
Haak Saxberg <haak.erling@gmail.com>
2011-05-22 04:15:01 -0700

    woo problems with things untestable until deployment\!

--- m2m/faq/views.py
+++ m2m/faq/views.py
@@ -22,7 +22,7 @@ class FAQForm(forms.Form):
                 ]
     
     def __init__(self):
-        super(FAQForm,self).__init__()
+        super(self).__init__()
         
         self.fields['question'] = forms.CharField(widget=forms.Textarea(attrs={
                     'rows':'9',
