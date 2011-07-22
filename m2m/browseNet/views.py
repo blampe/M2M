2124414227cd19bf10e30a9ac759a4fe66799832
Haak Saxberg <haak@wl-201-127.cine.hmc.edu>
2011-07-22 14:28:22 -0700

    haha, added a new undec-var error, fixing the last one.

--- m2m/browseNet/views.py
+++ m2m/browseNet/views.py
@@ -165,7 +165,7 @@ def deepBrowse(request,type="Q",id=-1):
         
         folderList = Path.objects.filter(ppid=id, pid__gte=1)
         fileList = File.objects.filter(path=id)
-        host = path.hid.hid
+        host = folder.hid.hid
         
     else:
         return HttpResponseRedirect(reverse('browseNet.views.listAll',args=(1,)))
