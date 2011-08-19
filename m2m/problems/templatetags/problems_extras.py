8d2e27befb6dc90883d0311724d5ffcdb2f5cdbf
Haak Saxberg <haak.erling@gmail.com>
2011-08-18 23:29:09 -0700

    are you fucking serious? think i found the source of the UEE (UnicodeEncodeError)

--- m2m/problems/templatetags/problems_extras.py
+++ m2m/problems/templatetags/problems_extras.py
@@ -8,8 +8,8 @@ def lenlimit(s, size):
     try:
         size = int(size)
     except:
-        return "??"
+        return u"??"
     s = str(s)
     if len(s) > size:
-        s = "{}...{}".format(s[:size/2],s[-size/2:])
+        s = u"{}...{}".format(s[:size/2],s[-size/2:])
     return s
