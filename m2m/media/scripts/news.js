ab6dc73e8474cb1d2d8b8dc8ab98254d6e9cd676
Haak Saxberg <haak.erling@gmail.com>
2011-09-17 18:16:55 -0700

    fixed some issues with the new interface

--- m2m/media/scripts/news.js
+++ m2m/media/scripts/news.js
@@ -13,6 +13,40 @@ function dispToggle(id){
         teaser.style.display = 'block';
     }
 }
+function bindPostCommentHandler(){
+    $('#commentForm form').submit(function(){
+       $.ajax({
+            type: "POST",
+            data: $('#commentForm form').serialize(),
+            url: "{%comment_form_target%}",
+            cache: false,
+            dataType: "html",
+            success: function(html, textStatus){
+                $('#commentForm form').replaceWith("Thanks for your comment!");
+                bindPostCommentHandler();
+                $('#commentContainer').load('{{object.get_absolute_url}} #commentLister')
+            },
+            error: function( XMLHttpRequest,textStatus,errorThrown){
+                if($('#commentForm form #id_comment').value == ''){
+                    $('#commentForm form #id_comment').value = "Please write a comment!";
+                }
+            }
+        });
+       return false;
+    });
+}
+
+var edited = false;
+    function toggleText(id){
+        if($(id)){
+            var $target = $(id);
+            if($target.attr('value') == "Anonymous" && edited == false){
+                $target.attr('value', "");
+            } else if(edited == false){
+                $target.attr('value',"Anonymous");
+            }
+        }
+    }
 
 $(function(){
     // attach toggles to togglebots
@@ -22,5 +56,17 @@ $(function(){
         $(this).css('marginBottom',-$(this).height());
         return -$(this).width() - 10 - parseInt($(".postWrap,.detailwrap").css('padding-left'));
     });
-
+    
+    
+    // news comment form
+    $("#id_comment").width(function(){
+        return $(this).parents("td:first").width()
+    });
+    
+    $("#submitcomment").click(function(){
+        $(this).parents("form:first").submit();
+        return false;
+    });
+    
+    bindPostCommentHandler();
 });
